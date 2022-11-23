4e4deabce9a17036af6b91b3d25ee21a77b406fa
tofaiko <65387701+tofaiko@users.noreply.github.com>
2022-11-23 19:30:29 +0900

    一番シンプルな時計

--- kadaiC/clock.js
+++ kadaiC/clock.js
@@ -1,1 +1,28 @@
+// 時間を取得する
+function getTime() {
+    var now = new Date();
+    var hh = 0;
+    var mm = 0;
+    var ss = 0;
 
+    if (now.getHours() < 10) {
+        hh = '0' + now.getHours();
+    } else {
+        hh = now.getHours();
+    }
+    if (now.getMinutes() < 10) {
+        mm = '0' + now.getMinutes();
+    } else {
+        mm = now.getMinutes();
+    }
+    if (now.getSeconds() < 10) {
+        ss = '0' + now.getSeconds();
+    } else {
+        ss = now.getSeconds();
+    }
+    nowtime = hh + ":" + mm + ":" + ss;
+    var msg = document.getElementById('pos');
+    document.getElementById('clock-show').innerHTML = msg;
+}
+
+setInterval('getTime()', 1000)
